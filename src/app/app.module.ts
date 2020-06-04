0686246f11e99ed86764fdf4852c20d764d05d79
Yuri Martins <cyuri.martins@gmail.com>
2020-06-04 02:16:11 -0300

    commit all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,44 @@
-import { environment } from './../environments/environment';
+import { environment } from "./../environments/environment";
 
+import { ComponentsModule } from "./components/components.module";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouteReuseStrategy } from "@angular/router";
 
-import { ComponentsModule } from './components/components.module';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
-
-
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireDatabase } from '@angular/fire/database';
-import { HttpModule, Http } from '@angular/http';
-import { AngularFirestoreModule} from '@angular/fire/firestore';
-import { AngularFireAuthModule} from '@angular/fire/auth';
-
-
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireDatabase } from "@angular/fire/database";
+import { HttpModule, Http } from "@angular/http";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { AngularFireAuthModule } from "@angular/fire/auth";
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, HttpModule, AngularFirestoreModule, 
-            AngularFireModule.initializeApp(environment.firebase),
-             AngularFireAuthModule, 
-    
-    IonicModule.forRoot(), AppRoutingModule,ComponentsModule
-         
+  imports: [
+    BrowserModule,
+    HttpModule,
+    AngularFirestoreModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    ComponentsModule,
   ],
   providers: [
     StatusBar,
-       SplashScreen,
-       AngularFireModule,
-       AngularFireDatabase,
-      
-       
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    SplashScreen,
+    AngularFireModule,
+    AngularFireDatabase,
+
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
